c9f8cffb9a392177235764d4983415beef4df10a
Naufal Zayn M <Naufal Zayn M@DESKTOP-3CT6V68>
2018-08-01 03:24:13 +0700

    Merge origin/master
    Conflicts:
    	src/kemahasiswaan_10116514_10116517/frm_nilai_mhs.java

---
+++ src/kemahasiswaan_10116514_10116517/Kemahasiswaan_10116514_10116517.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kemahasiswaan_10116514_10116517;
+
+/**
+ *
+ * @author Naufal Zayn M
+ */
+public class Kemahasiswaan_10116514_10116517 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
